abb56947df23604f4afd42ea02b457331a499c0d
Leonardo Felipe Dos Santos <leonardo.flss@hotmail.com>
2021-09-22 18:40:52 -0300

    Solucao Implicita...nao converge

--- lista1/exercicio6i.py
+++ lista1/exercicio6i.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 from numpy.core.fromnumeric import mean, shape, size
+from numpy.core.shape_base import block
 
 lmb = 0.01      # Difusividade Termica
 cond = 1.00     # Condutividade Termica
-dt = 0.01       # Time step
-dx = 0.02       # Length step
+dt = 0.02       # Time step
+dx = 0.03       # Length step
 
 A1 = lmb*dt/(dx**2)
 A2 = 1 - 2*lmb*dt/(dx**2)
@@ -30,7 +31,7 @@ pos_x = np.arange(xi, xf+dx, dx)
 
 Tp = np.ones([size(pos_x), size(tempo)]) * Tp_t0
 
-## C.C.
+## C.C. (Neumann)
 q_xf = np.zeros(size(tempo))
 q_xi = np.zeros(size(tempo))
 for i in range(size(tempo)):
@@ -55,12 +56,53 @@ for k in range(size(tempo)-1):
 # Sanity Check: Tp media em tf deve ser maior que Tp de t0,
 # visto que calor entra na barra e apos 10s ela esta completamente adiabatica
 print(mean(Tp[:,-1]))
+SolExp_Tp = Tp.copy()
+
+# Reiniciando as Temperaturas
+Tp = np.ones([size(pos_x), size(tempo)]) * Tp_t0
+
+## Abordagem Implicita:
+# Montando a matrix A do sistema linear:
+ofst = 1
+d1 = -A1 * np.ones(size(pos_x)-ofst)
+d2 =  A2 * np.ones(size(pos_x))
+d3 = -A3 * np.ones(size(pos_x)-ofst)
+
+A = np.diag(d2, 0)
+A = A + np.diag(d1, ofst)
+A = A + np.diag(d3,-ofst)
+# CC:
+A[0,:] =  0
+A[0,0] = -1
+A[0,2] =  1
+
+A[-1, :] =  0
+A[-1,-1] =  1
+A[-1,-3] = -1
+
+#print(A)
+
+# A solucao do sistema linear inverte A. Como A é constante ao longo do tempo,
+# invertemos fora do loop apenas uma vez para simplificacao computacional:
+Ainv = np.linalg.inv(A)
+
+for k in range(size(tempo)-1):
+    b = Tp[:,k].copy()
+    #b[0]  = -2*dx/cond*q_xi[k+1]
+    #b[-1] = -2*dx/cond*q_xf[k+1]
+    Tp[:,k+1] = np.dot(Ainv,b)
+    Tp[0,k+1] = Tp[2,k+1] + 2*dx/cond*q_xi[k+1]
+    Tp[-1,k+1] = Tp[-3,k+1] - 2*dx/cond*q_xi[k+1]
+#endfor
+print(mean(Tp[:,-1]))
+SolImp_Tp = Tp.copy()
+
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+tempo, pos_x = np.meshgrid(tempo, pos_x)
 
 # Plot the surface.
-tempo, pos_x = np.meshgrid(tempo, pos_x)
-surf = ax.plot_surface(pos_x, tempo, Tp, cmap=cm.coolwarm,
+surf = ax.plot_surface(pos_x, tempo, SolExp_Tp, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 # Customize the z axis.
@@ -72,4 +114,23 @@ ax.zaxis.set_major_formatter('{x:.02f}')
 # Add a color bar which maps values to colors.
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
-plt.show()
+plt.show(block=False)
+
+SolImp_Tp = SolExp_Tp.copy()                                # comentar aqui
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Plot the surface.
+surf = ax.plot_surface(pos_x, tempo, SolImp_Tp, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(1.00, 1.50)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show(block=True)
