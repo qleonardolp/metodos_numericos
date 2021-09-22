aecbd142a755bf7479e22982c6456af5d027e5f1
Leonardo Felipe Dos Santos <leonardo.flss@hotmail.com>
2021-09-22 14:47:46 -0300

    Revisado 6i, corrigido CI e add sanity check

--- lista1/exercicio6i.py
+++ lista1/exercicio6i.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
-from numpy.core.fromnumeric import shape, size
+from numpy.core.fromnumeric import mean, shape, size
 
 lmb = 0.01
 cond = 1.00
@@ -28,8 +28,7 @@ Tp_t0 = 1   # C.I.
 tempo = np.arange(ti, tf+dt, dt)
 pos_x = np.arange(xi, xf+dx, dx)
 
-Tp = np.zeros([size(pos_x), size(tempo)])
-Tp[:,0] = Tp_t0
+Tp = np.ones([size(pos_x), size(tempo)]) * Tp_t0
 
 ## C.C.
 q_xf = np.zeros(size(tempo))
@@ -44,14 +43,17 @@ for i in range(size(tempo)):
 for k in range(size(tempo)-1):
     for i in range(size(pos_x)-1):
         if i == 0:
-            Tp[i,k] = 2*dx/cond*q_xi[k] + Tp[i+2,k] # CC em x=0
+            Tp[i,k] = 2*dx/cond*q_xi[k] + Tp[i+2,k]     # CC em x=0
         if i == (size(pos_x)-1):
-            Tp[i+1,k] = Tp[i-1,k]                   # CC em x=L
+            Tp[i+1,k] = 2*dx/cond*q_xf[k] + Tp[i-1,k]   # CC em x=L
         else:
             Tp[i,k+1] = A1*Tp[i+1,k] + A2*Tp[i,k] + A3*Tp[i-1,k]
         #endif
     #endfor
 #endfor
+
+# Sanity Check: Tp media em tf deve ser maior que Tp de t0:
+print(mean(Tp[:,-1]))
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
@@ -61,7 +63,7 @@ surf = ax.plot_surface(pos_x, tempo, Tp, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 # Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
+ax.set_zlim(1.00, 1.50)
 ax.zaxis.set_major_locator(LinearLocator(10))
 # A StrMethodFormatter is used automatically
 ax.zaxis.set_major_formatter('{x:.02f}')
