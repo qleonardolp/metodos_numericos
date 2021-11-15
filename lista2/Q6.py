0dffffa1bc5b684ce85434975b9296bd6b67cdee
Leonardo Felipe Dos Santos <leonardo.flss@hotmail.com>
2021-11-15 15:56:07 -0300

    Corrigido pontos de int e pesos, usando os valores tabelados de Gauss-Lagrange

--- lista2/Q6.py
+++ lista2/Q6.py
@@ -6,6 +6,7 @@
 
 ## Integral da funcao de Bessel (0) via quadratura gaussiana.
 # Resultado pelo WolframAlpha: 1.3875672520098649871
+# Resultado para N=30          1.3875672520098639
 
 import numpy as np
 from scipy import special as sp
@@ -21,33 +22,19 @@ for N in range(30):
     poly_ord = 2*n_gp - 1
     dim = poly_ord + 1
 
-    x    = np.linspace(Linf, Lsup, n_gp)
-    eta  = np.zeros(n_gp)
+    # sample points and weights for Gauss-Legendre quadrature:
+    eta, W_vec  = np.polynomial.legendre.leggauss(n_gp) 
     fdex = np.zeros(n_gp)
-    # linear mapping
-    for i, xk in enumerate(x):
-        eta[i]  = (2*xk - (Linf + Lsup))/(Lsup - Linf)
+    
+    for i, eta_i in enumerate(eta):
+        xk = 0.5*(Lsup + Linf) + 0.5*eta_i*(Lsup - Linf)
         fdex[i] = sp.jv(0,xk)   # Funcao de Bessel do primeiro tipo (J_0)
-
-    P_vec = np.zeros(dim)
-    W_vec = np.zeros(n_gp)
-    M_mtx = np.zeros((n_gp,dim))
-
-    # construcao de ^P:
-    for i in range(dim):
-        P_vec[i] = (1**(i+1) - (-1)**(i+1))/(i+1)
-
-    # construcao de [M]:
-    for i in range(n_gp):
-        for j in range(dim):
-            M_mtx[i,j] = eta[i]**(j)
-
-    W_vec = np.matmul( np.linalg.pinv(M_mtx.T), P_vec) # pseudoinversa pois M nao eh quadrada
 
     Integral[N] = 0.5*(Lsup - Linf)*np.dot(W_vec, fdex)
 #endfor
 
-#quit()
+print(Integral[-1])
+
 #Plotting
 plt.figure()
 N = np.linspace(1,30,30)
@@ -55,7 +42,8 @@ plt.plot(N, Integral,'--b')
 plt.xlabel(r'$n$')
 plt.ylabel(r'$\int J_0(x) dx$')
 plt.grid()
-plt.show(block=False)
+plt.show(block=True)
+quit()
 
 plt.figure()
 N = np.linspace(10,30,20)
