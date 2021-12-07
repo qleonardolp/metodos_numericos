847d106fab7edd711c877d476a82ea7bb22accab
Leonardo Felipe Dos Santos <leonardo.flss@hotmail.com>
2021-12-06 23:01:29 -0300

    Arrumando codigo

--- lista2/Q8.py
+++ lista2/Q8.py
@@ -10,32 +10,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from scipy import sparse
-from scipy.sparse.linalg import spsolve
-
+from scipy.sparse import linalg as sprlinalg
 ## Class Definition
 
 class fem1DHTTransient():
-    def __init__(self, temp_init, end_time):
+    def __init__(self, temp_init, end_time, time_steps):
         self.nodes = []
         self.elements = []
         self.nnodes = 0
         self.nelements = 0
         self.temp_begin = temp_init
         self.end_time   = end_time
-        self.num_steps = 150
+        self.num_steps = time_steps
         self.time = np.linspace(0.0, self.end_time, self.num_steps)
 
     def createNodes(self, coords):
         self.nodes = coords
         self.nnodes = len(self.nodes)
 
-    def createElements(self, connectivities, condutividade, rhocp, area):
+    def createElements(self, connectivities):
         self.connectivities = connectivities
-        for k,item in enumerate(connectivities):
-            element = HT2(item, condutividade[k], rhocp[k], area[k])
+        for item in connectivities:
+            element = HT2(item)
             self.elements.append(element)
-
         self.nelements = len(self.elements)
+
+    def defineProperties(self, props):
+        for element in self.elements:
+            element.k = props['condutividade']
+            element.rhocp = props['cap_termica_vol']
 
     def createBoundaryConds(self, conditions):
         self.bcs_nodes = [] # id/ tipo/ valor/ tempo
@@ -48,40 +51,37 @@ class fem1DHTTransient():
                 typ, val, time = conditions['D']
                 self.bcs_nodes.append((i, typ, val, time))
 
-
     def solve(self):
 
-        rowsk = []
-        colsk = []
-        valuesk = []
-        rowsm = []
-        colsm = []
-        valuesm = []
-        area = []
+        rowsK = []
+        colsK = []
+        valuesK = []
+        rowsM = []
+        colsM = []
+        valuesM = []
 
         for element in self.elements:
             r, c, v = element.Ke(self.nodes)
-            rowsk.append(r)
-            colsk.append(c)
-            valuesk.append(v)
+            rowsK.append(r)
+            colsK.append(c)
+            valuesK.append(v)
             r, c, v = element.Me(self.nodes)
-            rowsm.append(r)
-            colsm.append(c)
-            valuesm.append(v)
-            area.append(element.A)
+            rowsM.append(r)
+            colsM.append(c)
+            valuesM.append(v)
 
-        rowsk = np.array(rowsk, dtype='int').flatten()
-        colsk = np.array(colsk, dtype='int').flatten()
-        valuesk = np.array(valuesk, dtype='float').flatten()
-        rowsm = np.array(rowsm, dtype='int').flatten()
-        colsm = np.array(colsm, dtype='int').flatten()
-        valuesm = np.array(valuesm, dtype='float').flatten()
-
+        rowsK = np.array(rowsK, dtype='int').flatten()
+        colsK = np.array(colsK, dtype='int').flatten()
+        valuesK = np.array(valuesK, dtype='float').flatten()
         # Montagem da matriz global K
-        Kglobal = sparse.csr_matrix((valuesk, (rowsk, colsk)), shape=((self.nnodes, self.nnodes)))
+        Kglobal = sparse.csr_matrix((valuesK, (rowsK, colsK)), shape=((self.nnodes, self.nnodes)))
         Kglobal = Kglobal + Kglobal.T - sparse.diags(Kglobal.diagonal(), dtype='float')
+
+        rowsM = np.array(rowsM, dtype='int').flatten()
+        colsM = np.array(colsM, dtype='int').flatten()
+        valuesM = np.array(valuesM, dtype='float').flatten()
         # Montagem da matriz global M
-        Mglobal = sparse.csr_matrix((valuesm, (rowsm, colsm)), shape=((self.nnodes, self.nnodes)))
+        Mglobal = sparse.csr_matrix((valuesM, (rowsM, colsM)), shape=((self.nnodes, self.nnodes)))
         Mglobal = Mglobal + Mglobal.T - sparse.diags(Mglobal.diagonal(), dtype='float')
 
         self.Kglobal = Kglobal
@@ -105,29 +105,27 @@ class fem1DHTTransient():
         Tp = np.zeros((self.num_steps, self.nnodes))
         Tp[0,:] = self.temp_begin # condicao de temperatura inicial
 
-        timestep_1 = self.num_steps/(self.end_time - 0.0) # inverso de Delta_t
+        Dt = self.end_time/self.num_steps # Delta_t
 
         #Metodo Implicito
-        Ak1 = (timestep_1*Mglobal + Kglobal).toarray() # precisa converter para usar inv
-        Ak1inv = np.linalg.inv(Ak1)
-        Ak = np.matmul(Ak1inv, timestep_1*Mglobal.toarray()) # precisa converter para usar inv
+        Ak1 = ((1/Dt)*Mglobal + Kglobal) # precisava converter para usar inv da numpy: ".toarray()"
+        Ak1_inv = sprlinalg.inv(Ak1.tocsr())
+        Ak = Ak1_inv*(1/Dt)*Mglobal
 
         for k, t in enumerate (self.time[:-1]):
-            # ajustando o vetor fglobal de acordo com BC de Dirichlet
+            # ajustando o vetor fglobal de acordo com BC de Neumann
             for nd in self.bcs_nodes:
-                Ar = 1.00 # ajustar isso para usar area de cada elemento
-                if(nd[1] == 1): #BC de Dirichlet
+                if(nd[1] == 1): #BC de Neumann
                     if (nd[3] > 0) and (t <= nd[3]):
-                        fglobal[nd[0]] = Ar*nd[2]
+                        fglobal[nd[0]] = nd[2]
                     if (nd[3] > 0) and (t > nd[3]):
                         fglobal[nd[0]] = 0
                     if (nd[3] == -1):
-                        fglobal[nd[0]] = Ar*nd[2]
-
-            Tp[k+1,:] = np.matmul(Ak,Tp[k,:]) + np.matmul(Ak1inv, fglobal)
+                        fglobal[nd[0]] = nd[2]
+            # metodo implicito:
+            Tp[k+1,:] = Ak*Tp[k,:] + Ak1_inv*fglobal
 
         self.T = Tp
-
 
     def plot(self):
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -145,17 +143,17 @@ class fem1DHTTransient():
 
 
 class HT2():
-    def __init__(self, nodes, k, rhocp, area):
+    def __init__(self, nodes):
         self.enodes = nodes
-        self.k = k
-        self.rhocp = rhocp
-        self.A = area
+        self.rhocp = 0
+        self.k = 0
 
     def Ke(self, coords): # constroi a matrix K do elemento
         x = coords[self.enodes, 0]
         self.L = abs(x[1] - x[0])
+        Area = 1 # area unitaria
 
-        Ke_11 = (self.k * self.A / self.L) 
+        Ke_11 = (self.k *Area / self.L) 
         Ke_12 = -Ke_11
         Ke_22 =  Ke_11
         
@@ -176,7 +174,6 @@ class HT2():
         Ke_12 = Ke_11/2
         Ke_22 = Ke_11
         
-        # Ke = [[(n1, n1), (n1, n2)], [(n2, n1), (n2,n2)]]
         n1 = self.enodes[0]
         n2 = self.enodes[1]
         ind_rows = [n1, n1, n2]
@@ -190,27 +187,24 @@ class HT2():
 Ti = 1.000      #[°C]
 L_barra = 1.000 #[m]
 t_end = 30.00   #[s]
-#alpha = 1/(1.00*100) # k/rho*c [m^2/s]
-condutividade_termica = 1.000            #[W/(m^2 K)]
-cap_termica_vol = 1.00*100.00   #[J/(m^3 K)]
-A = 1.00 # area da secao transversal
+k_term = 1.000   #[W/(m^2 K)]
+cap_term_vol = 1.00*100.00   #[J/(m^3 K)]
+time_steps = 150
 nnodes = 16
 # util para o caso 2D:
 coords = np.zeros((nnodes, 2)) 
 coords[:,0] = np.linspace(0.0, L_barra, nnodes)
-# 2 nos por elemento
+# 2 nos por elemento:
 connectivities = np.zeros((nnodes - 1, 2), dtype='int')
 connectivities[:,0] = np.arange(0, nnodes-1)
 connectivities[:,1] = np.arange(1, nnodes)
 
-cond = condutividade_termica*np.ones((nnodes - 1, 1))
-rhocp = cap_termica_vol*np.ones((nnodes - 1, 1))
-areas = A*np.ones((nnodes - 1, 1))
-
-problem = fem1DHTTransient(Ti, t_end)
+problem = fem1DHTTransient(Ti, t_end, time_steps)
 problem.createNodes(coords) # array com coords dos nos
-problem.createElements(connectivities, cond, rhocp, areas) 
-# array dos elementos, em que cada elemento contem os ids dos seus nos, mas o objeto element nao contem a info das coords
+problem.createElements(connectivities) # array dos elementos, 
+# em que cada elemento contem os ids dos seus nos, mas o objeto element nao contem a info das coords
+props = {'condutividade':k_term, 'cap_termica_vol':cap_term_vol}
+problem.defineProperties(props)
 
 # Boundary conditions
 qe  = 1.00
